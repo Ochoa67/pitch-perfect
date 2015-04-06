74940d2d6307b41af0614a82559d6bd07b8e1c73
Ochoa67 <ochoa67@me.com>
2015-04-05 22:00:54 -0500

    Committed by Udacity

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Jose Ochoa on 3/24/15.
+//  Copyright (c) 2015 Primattebrain. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject {
+    var filePathUrl: NSURL!
+    var title: String!
+    
+}
